f68e7ada03b75b0e7f560442edf673cb27da91af
abigahil.delgado <abigahil.delgado@ine.mx>
2022-11-21 09:57:54 -0600

    feat: inicio de ejemplo

---
+++ src/components/Create.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'semantic-ui-react'
+import axios from 'axios';
+
+export default function Create() {
+
+    const [name, setName] = useState('');
+    const [age, setAge] = useState();
+    const [height, setHeight] = useState();
+    const [phone, setPhone] = useState();
+
+    const postData = () => {
+        console.log(name);
+        console.log(age);
+        console.log(height);
+        console.log(phone);
+
+        axios.post(`/person`, {
+            name, age, height, phone
+        });
+    }
+
+    return (
+        <div>
+            <Form className='create-form'>
+        <Form.Field>
+            <label>Name</label>
+            <input placeholder='Name' onChange={ (e) => setName(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <label>Age</label>
+            <input placeholder='Age' onChange={(e) => setAge(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <label>Height</label>
+            <input placeholder='Height' onChange={(e) => setHeight(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+            <label>Phone</label>
+            <input placeholder='Phone' onChange={(e) => setPhone(e.target.value)} />
+        </Form.Field>
+        <Button onClick={postData} type='submit'>Submit</Button>
+    </Form>
+        </div>
+    )
+}
